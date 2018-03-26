eb17cc98a40ebd2437c581926a423a49b2e80bc7
patricia <patrigonzalez28@gmail.com>
2018-03-26 21:09:55 +0200

    metodo utils sin terminar arrggggg

--- core/src/com/mygdx/game/Utils.java
+++ core/src/com/mygdx/game/Utils.java
@@ -1,26 +1,31 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.mygdx.game.entities.AsteroidEntity;
+import com.mygdx.game.entities.EntityFactory;
+
+import java.util.ArrayList;
 
 /**
  * Created by patry on 23/03/2018.
  */
 
-public class Utils {
-    private World world;
-    private Texture texture;
-    private Vector2 initialPosition;
-    private Vector2 impulse;
-    private Float radius;
+public class Utils extends Actor {
+    private static EntityFactory factory;
+    World world;
+    Texture texture;
+    Float radius = 1f;
+    Vector2 position;
+    Vector2 impulse;
 
-
-    public void randomAsteroids(World world, Texture texture){
-        this.world = world;
-        this.texture = texture;
+    public Utils() {
         //TODO: terminar
 
 
     }
 }
+
